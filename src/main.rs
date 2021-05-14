0163815b2bcf998cc2d86e7f44c5b49d00a1eb15
Francis Holtken <dev@arkeo.ca>
2021-05-14 14:24:23 -0400

    Parse command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use argparse::{ArgumentParser, StoreTrue, Store, StoreOption, Print};
+
 fn main() {
-    println!("Hello World");
+    let mut file: String = "".to_string();
+
+    let mut delete = false;
+    let mut force = false;
+    let mut ignore = false;
+    let mut meta = false;
+    let mut showmeta = false;
+
+    let mut jsonmeta: Option<String> = None;
+    let mut key: Option<String> = None;
+    let mut name: Option<String> = None;
+    let mut outfile: Option<String> = None;
+
+
+    {
+        let mut ap = ArgumentParser::new();
+
+        ap.refer(&mut file).add_argument("file", Store, "");
+        ap.add_option(&["-v", "--version"], Print(format!("CaRT v{} (Rust)", env!("CARGO_PKG_VERSION").to_string())), "Show program's version number and exit");
+        ap.refer(&mut delete).add_option(&["-d", "--delete"], StoreTrue, "Delete original after operation succeeded");
+        ap.refer(&mut force).add_option(&["-f", "--force"], StoreTrue, "Replace output file if it already exists");
+        ap.refer(&mut ignore).add_option(&["-i", "--ignore"], StoreTrue, "Ignore RC4 key from conf file");
+        ap.refer(&mut jsonmeta).add_option(&["-j", "--jsonmeta"], StoreOption, "Provide header metadata as JSON blob");
+        ap.refer(&mut key).add_option(&["-k", "--key"], StoreOption, "Use private RC4 key (base64 encoded). Same key must be provided to unCaRT.");
+        ap.refer(&mut meta).add_option(&["-m", "--meta"], StoreTrue, "Keep metadata around when extracting CaRTs");
+        ap.refer(&mut name).add_option(&["-n", "--name"], StoreOption, "Use this value as metadata filename");
+        ap.refer(&mut outfile).add_option(&["-o", "--outfile"], StoreOption, "Set output file");
+        ap.refer(&mut showmeta).add_option(&["-s", "--showmeta"], StoreTrue, "Only show the file metadata");
+
+        ap.parse_args_or_exit();
+    }
 }
