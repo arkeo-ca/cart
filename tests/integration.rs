d3adc07cd8c15dbec262a9e158b508d2090f4245
Francis Holtken <dev@arkeo.ca>
2021-07-23 13:33:05 -0400

    Add small stream test

--- tests/integration.rs
+++ tests/integration.rs
@@ -1,4 +1,4 @@
-use json::JsonValue;
+use json::{JsonValue, object};
 use std::io::Cursor;
 
 #[test]
@@ -11,14 +11,29 @@ fn test_empty() {
     assert!(cart::is_cart(&cart_stream[..]));
 
     let mut cart_cur = Cursor::new(cart_stream);
-    let metadata = cart::unpack(&mut cart_cur, &mut final_stream, None);
-    assert_eq!(JsonValue::Null, metadata.unwrap());
-    assert_eq!(0, final_stream.len());
+    let (header, footer) = cart::unpack(&mut cart_cur, &mut final_stream, None).unwrap();
+    assert_eq!(JsonValue::new_object(), header);
+    assert_eq!(JsonValue::new_object(), footer);
+    assert_eq!(empty_stream, final_stream);
 }
 
 #[test]
 fn test_small() {
+    let small_stream: Vec<u8> = vec![61]; // "="
+    let mut cart_stream:Vec<u8> = Vec::new();
+    let mut final_stream: Vec<u8> = Vec::new();
 
+    let opt_header = object! {"testkey": "testvalue"};
+    let opt_footer = object! {"complete": "yes"};
+
+    cart::pack(&mut small_stream.as_slice(), &mut cart_stream, Some(opt_header.clone()), Some(opt_footer.clone()), None).unwrap();
+    assert!(cart::is_cart(&cart_stream[..]));
+
+    let mut cart_cur = Cursor::new(cart_stream);
+    let (header, footer) = cart::unpack(&mut cart_cur, &mut final_stream, None).unwrap();
+    assert_eq!(opt_header, header);
+    assert_eq!(opt_footer, footer);
+    assert_eq!(small_stream, final_stream);
 }
 
 #[test]
