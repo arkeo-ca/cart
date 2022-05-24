b0c21b6767cbb854c4b79233ac948822ed8bf86c
Francis Holtken <62658087+arkeo-ca@users.noreply.github.com>
2022-05-23 22:29:01 -0400

    Feature/use clap instead of argparse (#14)
    * Change over to clap
    
    * change positional argument name to reflect list status
    
    * remove debug print statement
    
    * fix issue where cart extension was not recognized

--- src/config.rs
+++ src/config.rs
@@ -1,11 +1,11 @@
 use std::path::Path;
 use configparser::ini::Ini;
-use argparse::{ArgumentParser, StoreTrue, List, StoreOption, Print};
 use json::JsonValue;
+use clap::Parser;
 
 #[derive(Debug)]
 pub struct Config {
-    pub file: Vec<String>,
+    pub files: Vec<String>,
     pub delete: bool,
     pub force: bool,
     pub ignore: bool,
@@ -20,19 +20,8 @@ pub struct Config {
 
 impl Config {
     pub fn new() -> Result<Config, Box<dyn std::error::Error>> {
-        let mut file = Vec::new();
-
-        let mut delete = false;
-        let mut force = false;
-        let mut ignore = false;
-        let mut meta = false;
-        let mut showmeta = false;
-
-        let mut jsonmeta: Option<String> = None;
-        let mut key: Option<String> = None;
-        let mut name: Option<String> = None;
-        let mut outfile: Option<String> = None;
-
+        let mut args = Args::parse();
+        
         let mut default_header = JsonValue::new_object();
         {
             #[cfg(target_os = "linux")]
@@ -47,45 +36,78 @@ impl Config {
 
             if let Ok(m) = map {
                 if let Some(v) = config.getbool("global", "keep_meta").unwrap() {
-                    meta = v;
+                    if !args.meta {
+                        args.meta = v;
+                    }
                 }
                 if let Some(v) = config.getbool("global", "force").unwrap() {
-                    
-                    force = v;
+                    if !args.force {
+                        args.force = v;
+                    }
                 }
                 if let Some(v) = config.getbool("global", "delete").unwrap() {
-                    delete = v;
+                    if !args.delete {
+                        args.delete = v;
+                    }
                 }
-                key = config.get("global", "rc4_key");
+                if args.key == None {
+                    args.key = config.get("global", "rc4_key");
+                }
+                
                 if m.contains_key("default_header") {
                     default_header = json::from(m["default_header"].clone());
                 }
             }
         }
 
-        {
-            let mut ap = ArgumentParser::new();
-
-            ap.refer(&mut file).add_argument("file", List, "");
-            ap.add_option(&["-v", "--version"], Print(format!("CaRT v{} (Rust)", env!("CARGO_PKG_VERSION").to_string())), "Show program's version number and exit");
-            ap.refer(&mut delete).add_option(&["-d", "--delete"], StoreTrue, "Delete original after operation succeeded");
-            ap.refer(&mut force).add_option(&["-f", "--force"], StoreTrue, "Replace output file if it already exists");
-            ap.refer(&mut ignore).add_option(&["-i", "--ignore"], StoreTrue, "Ignore RC4 key from conf file");
-            ap.refer(&mut jsonmeta).add_option(&["-j", "--jsonmeta"], StoreOption, "Provide header metadata as JSON blob");
-            ap.refer(&mut key).add_option(&["-k", "--key"], StoreOption, "Use private RC4 key (base64 encoded). Same key must be provided to unCaRT.");
-            ap.refer(&mut meta).add_option(&["-m", "--meta"], StoreTrue, "Keep metadata around when extracting CaRTs");
-            ap.refer(&mut name).add_option(&["-n", "--name"], StoreOption, "Use this value as metadata filename");
-            ap.refer(&mut outfile).add_option(&["-o", "--outfile"], StoreOption, "Set output file");
-            ap.refer(&mut showmeta).add_option(&["-s", "--showmeta"], StoreTrue, "Only show the file metadata");
-
-            ap.parse_args_or_exit();
+        if args.ignore {
+            args.key = None;
         }
 
-        if ignore {
-            key = None;
-        }
-
-        Ok(Config {file, delete, force, ignore, meta, showmeta, jsonmeta, key, name, outfile, default_header})
+        Ok(Config {files: args.files, delete: args.delete, force: args.force, ignore: args.ignore, 
+            meta: args.meta, showmeta: args.showmeta, jsonmeta: args.jsonmeta, key: args.key, 
+            name: args.name, outfile: args.outfile, default_header})
     }
 }
 
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    files: Vec<String>,
+
+    /// Replace output file if it already exists
+    #[clap(short, long)]
+    force: bool,
+
+    /// Ignore RC4 key from conf file
+    #[clap(short, long)]
+    ignore: bool,
+
+    /// Keep metadata around when extracting 
+    #[clap(short, long)]
+    meta: bool,
+
+    /// Delete original after operation succeeded
+    #[clap(short, long)]
+    delete: bool,
+
+    /// Only show the file metadata
+    #[clap(short, long)]
+    showmeta: bool,
+
+    /// Provide header metadata as json blob
+    #[clap(short, long)]
+    jsonmeta: Option<String>,
+    
+    /// Use private RC4 key (base64 encoded). Same key must be provided to unCaRT.
+    #[clap(short, long)]
+    key: Option<String>,
+    
+    /// Use this value as metadata filename
+    #[clap(short, long)]
+    name: Option<String>,
+    
+    /// Set output file
+    #[clap(short, long)]
+    outfile: Option<String>,
+}
